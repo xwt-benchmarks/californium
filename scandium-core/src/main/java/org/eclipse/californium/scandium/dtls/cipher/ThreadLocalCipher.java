e9d9937ed5f93a95b8f7945c657b035821b2f367
xingweitian <xingweitian@gmail.com>
2020-04-26 21:50:23 -0400

    Add annotation.

--- scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/cipher/ThreadLocalCipher.java
+++ scandium-core/src/main/java/org/eclipse/californium/scandium/dtls/cipher/ThreadLocalCipher.java
@@ -15,6 +15,8 @@
  ******************************************************************************/
 package org.eclipse.californium.scandium.dtls.cipher;
 
+import org.checkerframework.checker.crypto.qual.AllowedAlgorithms;
+
 import java.security.GeneralSecurityException;
 
 import javax.crypto.Cipher;
@@ -35,7 +37,7 @@ public class ThreadLocalCipher extends ThreadLocalCrypto<Cipher> {
 	 * @param transformation transformation. Passed to
 	 *            {@link Cipher#getInstance(String)}.
 	 */
-	public ThreadLocalCipher(final String transformation) {
+	public ThreadLocalCipher(final @AllowedAlgorithms({"((PBEWith(SHA256|HmacSHA1|HmacSHA224|HmacSHA256|HmacSHA384|HmacSHA512)And(DESede|TripleDES|2KeyTripleDES|3KeyTripleDES|AES_128|AES_256))|AES|AES_128|AES_256|AESWrap|ARCFOUR|ECIES|RC5|RSA)\\/(NONE|CBC|GCM|CCM|CFB|CTR|CTS|OFB|PCBC)\\/(NoPadding|ISO10126Padding|OAEPPadding|PKCS1Padding|PKCS5Padding|PKCS7Padding|SSL3Padding|(OAEPWith(SHA-224|SHA-256|SHA-384|SHA-512)AndMGF1Padding))", "RSA", "RSA\\/ECB\\/(NoPadding|ISO10126Padding|OAEPPadding|PKCS1Padding|PKCS5Padding|PKCS7Padding|SSL3Padding|(OAEPWith(SHA-224|SHA-256|SHA-384|SHA-512)AndMGF1Padding))", "PBEWith(SHA256|HmacSHA1|HmacSHA224|HmacSHA256|HmacSHA384|HmacSHA512)And(DESede|TripleDES|2KeyTripleDES|3KeyTripleDES|AES_128|AES_256)"}) String transformation) {
 		super(new Factory<Cipher>() {
 
 			@Override
